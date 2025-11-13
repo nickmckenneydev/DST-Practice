0866a269f3b64d6895cde1c04fbfa3f113deb369
nickmckenneydev <nick.mckenney.dev@gmail.com>
2025-11-12 21:49:40 -0500

    practice link list

---
+++ main.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct node{
+int data;
+struct node* next;
+}node;
+
+void printList(node* head){
+ node* current;
+}
+int main(){
+   
+    node* newNode=NULL;
+    newNode = malloc(sizeof(node));
+
+printf("What size should the linked list be? ");
+int size = 0;
+int result = scanf("%d",&size);
+if(result==0){
+    while(fgetc(stdin)!='\n');
+}
+node *insertFront(node* head,int v)
+{
+    int userInput=0;
+    scanf("Enter value %d: %d",i,&userInput);
+    newNode->data=userInput;
+    newNode->next = NULL;
+}
+}
